71d7756c7ec081107dca442c7d438d9635be65d9
santiagombv <santiagombv@gmail.com>
2016-05-10 01:14:11 -0300

    add practico 6

---
+++ 06 practico/rutina06.R
@@ -0,0 +1,73 @@
+histogram(~ ca.pulm, data = fum)
+densityplot(~ ca.pulm | fuma, data = fum)
+
+#############################################################
+
+##Gráficos del paquete *ggplot2*
+
+fum <- read.table("C:/RD/fumadores.txt", header=T)
+#  fum <- read.table(file=file.choose(), header=TRUE)
+
+library(ggplot2)
+
+#utilizando ggplot
+ggplot(data = fum, aes(x = edad, y = ca.pulm)) + geom_point()    
+
+#notar como los gráficos se van acumulando
+g1 <- ggplot(data = fum, aes(x = edad, y = ca.pulm))  
+g2 <- g1 + geom_point()
+g2    
+
+#modificando algunos detalles comunes
+g1 <- ggplot(data = fum, aes(x = edad, y = ca.pulm, color = fuma)) 
+g2 <- g1 + geom_point(size=3, aes(shape=sexo)) 
+g3 <- g2 + xlab("Edad")  
+g4 <- g3 + ylab("Capacidad Pulmonar")
+g5 <- g4 + theme_bw()
+g5
+
+#modificando los colores 
+g1 <- ggplot(data = fum, aes(x = edad, y = ca.pulm, color = fuma)) 
+g2 <- g1 + scale_colour_manual(values=c("red", "black"))
+g3 <- g2 + geom_point(size=3) 
+g3  
+
+#modificando los colores con una escala continua
+g1 <- ggplot(data = fum, aes(x = edad, y = ca.pulm, color = alt)) 
+g2 <- g1 + geom_point(size=3) 
+g2  
+
+#modificando tamaños con una escala continua
+g1 <- ggplot(data = fum, aes(x = edad, y = ca.pulm, size = alt, color = fuma)) 
+g2 <- g1 + geom_point() 
+g2  
+
+#Faceting (división del la ventana gráfica como en lattice)
+g1 <- ggplot(data = fum, aes(x = edad, y = ca.pulm, color = fuma))
+g2 <- g1 + geom_point(size=3) + theme_bw()
+g3 <- g2 + facet_grid(. ~ sexo)
+g3
+
+g1 <- ggplot(data = fum, aes(x = edad, y = ca.pulm, color = fuma))
+
+g2 <- g1 + geom_point(size = 3) + theme_bw()
+g3 <- g2 + facet_grid(fuma ~ .) + scale_color_manual(values = c("red", "blue"))
+g3
+
+#agregar líneas de regresión
+g1 <- ggplot(data = fum, aes(x = edad, y = ca.pulm, color = fuma))
+g2 <- g1 + geom_point(size = 3) + theme_bw()
+g3 <- g2 + geom_smooth(method = "lm")
+g3
+
+#gráficos de cajas
+g1 <- ggplot(data = fum, aes(x = fuma, y = ca.pulm))
+g2 <- g1 + geom_boxplot()
+g2
+
+#histogramas
+g1 <- ggplot(data = fum, aes(x = ca.pulm))
+g2 <- g1 + geom_histogram(fill="red")
+g2
+
+#### END ####
