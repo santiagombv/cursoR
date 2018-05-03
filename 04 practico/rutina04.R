a4cf3dc09eeb9ad0430845027f91a39d81373055
santiagombv <santiagombv@gmail.com>
2018-05-03 18:09:06 -0300

    major changes. shrinkage

--- 04 practico/rutina04.R
+++ 04 practico/rutina04.R
@@ -1,6 +1,6 @@
 ##Caso 1.
-dat <- read.table("C:/RD/s_poly.txt", header = TRUE)
-#  dat <- read.table(file = file.choose(), header = TRUE)
+# complete la ruta al directorio en ...
+dat <- read.table(".../s_poly.txt", header = TRUE)
 
 # exploración de los datos
 layout(matrix(1:8,2,4))
@@ -15,10 +15,10 @@ dat <- dat[-101, ]
 # exploración bivariada de los datos
 pairs(dat)
 
-###DETECCIÓN DE LA COLINEALIDAD
-library(car)     		
+# Detección de la colinealidad
+library(car)
 
-#A) Gráficos SPLOM
+# A) Gráficos SPLOM
 scatterplotMatrix(~ largo.l.tot + largo.tubo + caliz.sup + caliz.med +
                     caliz.inf + labio.sup + labio.inf, #variables a graficar
                   reg.line=lm,            #añadir rectas de regresión a los gráficos
@@ -26,43 +26,49 @@ scatterplotMatrix(~ largo.l.tot + largo.tubo + caliz.sup + caliz.med +
                   diagonal = 'density',   #gráficos de la diagonal
                   data = dat)             #conjunto de datos 
 
-#B) Matrices de correlación
+# B) Matrices de correlación
 CORR <- cor(dat[, c("largo.l.tot", "largo.tubo", "caliz.sup",
                     "caliz.med", "caliz.inf", "labio.sup", "labio.inf")], 
             use = "complete.obs") 
 CORR
 
-#C)Factores de inflación de la varianza
-# primero construimos el modelo completo, sin interacciones
-
+# C) Factores de inflación de la varianza
 fit <- lm(azucar ~ largo.l.tot + largo.tubo + caliz.sup + caliz.med +
             caliz.inf + labio.sup + labio.inf, data = dat)
 vif(fit)	
 
-#Decisión: usar el criterio de r < 0.7 y parcialmente los vif,
-#descartamos el cáliz inferior y construimos un nuevo modelo
+# Decisión: usar el criterio de r < 0.7 y parcialmente los vif,
+# descartamos el cáliz inferior y construimos un nuevo modelo
 
 fit2 <- lm(azucar ~ largo.l.tot + largo.tubo + caliz.sup + caliz.med + labio.sup + labio.inf, data = dat)
 summary(fit2)
 anova (fit2)
+
 layout(matrix(1:4, 2, 2))
 plot(fit2)
 layout(1)
 
-#revisando su colinealidad nuevamente
+# revisando su colinealidad nuevamente
 vif(fit2)
 
-###SELECCIÓN DE MODELOS 
-##anova puede usarse para comparar dos modelos anidados
-fit2<-lm(azucar ~ largo.l.tot + largo.tubo + caliz.sup + caliz.med + labio.sup + labio.inf, data=dat)
-fit3<-lm(azucar ~ largo.tubo + caliz.sup + caliz.med + labio.sup + labio.inf, data=dat) #sin largo.l.tot
+# Selección de modelos. Best subset selection.
+# Construyendo todos los modelos posibles
+# (pueden utilizarse forward o backward cambiando  el argumento method)
+library(leaps)
+BS.fit <- regsubsets(azucar ~ largo.l.tot + largo.tubo + caliz.sup + caliz.med + labio.sup + labio.inf, data = dat, method = "exhaustive")
+BS.summary <- summary(BS.fit)
+BS.summary
 
-anova(fit3, fit2)
+plot(BS.summary$cp, xlab = "número de variables", ylab = "Cp", type ="l")
+plot(BS.summary$bic, xlab = "número de variables", ylab = "BIC", type ="l")
 
-##drop1 borra una variable del modelo a la vez y compara con el completo
-drop1(fit2, test="F")
+plot(BS.fit, scale = "Cp")
+plot(BS.fit, scale = "bic")
 
-## stepAIC (MASS) realiza una búsqueda automática del mejor modelo 
+coef(BS.fit, 3)
+coef(BS.fit, 1)
+
+# stepAIC (MASS) realiza una búsqueda automática del mejor modelo 
 library(MASS) 
 stepAIC(fit2, direction ="both") #para usar AIC
 
@@ -70,7 +76,7 @@ stepAIC(fit2, direction ="both") #para usar AIC
 n<-nrow(dat)
 stepAIC(fit2, direction="both", k=log(n))#ajustar el modelo final
 
-### uso básico de MuMIn
+# Model averaging. Uso básico de MuMIn
 library(arm)
 library(MuMIn)
 
@@ -86,5 +92,28 @@ top.mod
 
 AVG<-model.avg(top.mod)
 summary(AVG)
+
+# Selección de Modelos. Shrinkage
+# Lasso
+
+library(glmnet)
+x <- model.matrix(azucar ~ largo.l.tot + largo.tubo + caliz.sup + caliz.med + labio.sup + labio.inf, data = dat)[,-1]
+y <- dat$azucar
+
+lasso.mod <- glmnet(x, y, alpha = 1)
+plot(lasso.mod)
+coef(lasso.mod, s = 0.1) # ejemplo
+
+# Cross validation.
+grid <- 10^seq(10, -10, length = 100)
+cv.out <- cv.glmnet(x, y, alpha = 1, nfolds = 10, lambda = grid)
+plot(cv.out)
+
+grid <- 10^seq(0.5, -4, length = 100)
+cv.out <- cv.glmnet(x, y, alpha = 1, nfolds = 10, lambda = grid)
+plot(cv.out)
+
+cv.out$lambda.min
+coef(cv.out, s = "lambda.min")
 
 #### END ####
